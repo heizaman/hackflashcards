26cdab8d6bdd047b32d25016446fd8e24c88c6b9
Aman Garg <msgmeaman@gmail.com>
2020-10-14 20:23:54 +0530

    Frontend files

--- routes/frontend.js
+++ routes/frontend.js
@@ -10,4 +10,20 @@ router.get('/createdeck', function(req, res, next) {
   res.render('createdeck');
 });
 
+router.get('/createflashcard', function(req, res, next) {
+  res.render('createflashcard');
+});
+
+router.get('/decks', function(req, res, next) {
+  res.render('decks');
+});
+
+router.get('/flashcards', function(req, res, next) {
+  res.render('flashcards');
+});
+
+router.get('/revise', function(req, res, next) {
+  res.render('revise');
+});
+
 module.exports = router;
