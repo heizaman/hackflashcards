52f6c85a25bef3463dba41f3b46449f3ed32d75e
Harini <harininarayanan1012@gmail.com>
2020-10-15 12:49:01 +0530

    Adding page to see all flashcards in a deck

--- public/javascripts/flashcards.js
+++ public/javascripts/flashcards.js
@@ -2,12 +2,58 @@ $(document).ready(function(){
     $.ajax({
        type: "GET",
        contentType: "application/json",
-       url: '/apis/getFlashcards/:deckid',
+       url: `/apis/getFlashcards/${deckid}`,
        success: function(response) {
-          console.log(response);
+            console.log(response.status);
+            console.log(response.result);
+            showFlashcardsOfDeck(response.result);
        },
        error: function(xhr, status, err) {
           console.log(err.toString());
        }
     });
 });
+
+function showFlashcardsOfDeck(flashcards) {
+    for(var i=0; i<flashcards.length; i++) {
+        var cardFront = '<div class="card-body frontOfCard">'
+        var front = JSON.parse(flashcards[i].front);
+        if(front.heading) {
+            cardFront += `<h1>${front.heading}</h1>`;
+        }
+        if(front.subheading) {
+            cardFront += `<h3>${front.subheading}</h3>`;
+        }
+        if(front.para) {
+            cardFront += `<p>${front.para}</p>`;
+        }
+        if(front.img) {
+            cardFront += `<img src = ${front.img}>`;
+        }
+        cardFront += '</div>';
+        console.log(front);
+
+        var cardBack = '<div class="card-body backOfCard">'
+        var back = JSON.parse(flashcards[i].back);
+        if(back.heading) {
+            cardBack += `<h1>${back.heading}</h1>`;
+        }
+        if(back.subheading) {
+            cardBack += `<h3>${back.subheading}</h3>`;
+        }
+        if(back.para) {
+            cardBack += `<p>${back.para}</p>`;
+        }
+        if(back.img) {
+            cardBack += `<img src = ${back.img}>`;
+        }
+        cardBack += '</div>';
+        console.log(back);
+
+        var flashcard = '<div class="card">';
+        flashcard += cardFront;
+        flashcard += cardBack;
+        flashcard += '</div>';
+        $('#allcards').append(flashcard);
+    }
+}
